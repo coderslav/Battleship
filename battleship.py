36c3535e6758035a5cbebd17689e55171204c780
coderslav <coderslav>
2020-12-30 14:37:15 +0100

    Battleship v1.0.5: +drew BattleField

--- battleship.py
+++ battleship.py
@@ -1,14 +1,44 @@
+from random import randrange
+from random import choice
 class BattleField:
-    def __init__(self,  hide=False):
+    def __init__(self, hide=False):
         self.hide = hide
         self.field = [[' ', '1', '2', '3', '4', '5', '6'], ['1', 'O', 'O', 'O', 'O', 'O', 'O'],
                       ['2', 'O', 'O', 'O', 'O', 'O', 'O'], ['3', 'O', 'O', 'O', 'O', 'O', 'O'],
                       ['4', 'O', 'O', 'O', 'O', 'O', 'O'], ['5', 'O', 'O', 'O', 'O', 'O', 'O'],
                       ['6', 'O', 'O', 'O', 'O', 'O', 'O']]
+        self.radar = [[' ', '1', '2', '3', '4', '5', '6'], ['1', 'O', 'O', 'O', 'O', 'O', 'O'],
+                      ['2', 'O', 'O', 'O', 'O', 'O', 'O'], ['3', 'O', 'O', 'O', 'O', 'O', 'O'],
+                      ['4', 'O', 'O', 'O', 'O', 'O', 'O'], ['5', 'O', 'O', 'O', 'O', 'O', 'O'],
+                      ['6', 'O', 'O', 'O', 'O', 'O', 'O']]
 
-    def test_print(self, x, y):
-        self.field[x][y] = "X"
+    def get_field(self):
         return self.field
+
+    def get_radar(self):
+        return self.radar
+
+    def draw_field(self):
+        field_str = ""
+        for row in self.field:
+            field_str += f"\n "+" | ".join(row)+" |"
+        if self.hide:
+            field_str = field_str.replace("■", "O")
+        return field_str
+
+    def draw_radar(self):
+        field_str = ""
+        for row in self.radar:
+            field_str += f"\n "+" | ".join(row)+" |"
+        if self.hide:
+            field_str = field_str.replace("■", "O")
+        return field_str
+
+    def shoot(self, x, y):
+        pass
+
+    def busy(self):
+        pass
 
 
 class Ships:
@@ -17,9 +47,11 @@ class Ships:
         self.hp = hp
         self.orientation = orientation
 
+    def ship_init(self, ):
+        pass
+
 
 b = BattleField()
-b.test_print(2, 2)
-for i in b.field:
-    print(i)
+print(b.draw_field())
+
 
